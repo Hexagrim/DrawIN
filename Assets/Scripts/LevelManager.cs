7ad46c613165f2451cac4155eccde0a2a21ab454
Hexagrim <hexagrim3456@gmail.com>
2026-04-13 21:27:15 +0545

    Hour 7, MAKING MAKING ENENENE, making UI, transitions, level buttons and so much more, the whole animation thing took the whole hour :<

--- Assets/Scripts/LevelManager.cs
+++ Assets/Scripts/LevelManager.cs
@@ -1,15 +1,25 @@
+using System.Collections;
 using UnityEngine;
 
 public class LevelManager : MonoBehaviour
 {
-    
+    public Animator T_Anim;
     void Start()
     {
-        
+
     }
 
     void Update()
     {
-        
+
+    }
+
+    public void NextLevel()
+    {
+        StartCoroutine()
+    }
+    IEnumerator NextScene()
+    {
+
     }
 }
